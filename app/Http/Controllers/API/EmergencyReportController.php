f867cd21fa3257c5d1361f2c9e129b0d1345aeb0
Heri Hermawan <herihermawan@Heris-MacBook-Air.local>
2023-01-01 16:27:41 +0700

    update

--- app/Http/Controllers/API/EmergencyReportController.php
+++ app/Http/Controllers/API/EmergencyReportController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use DB;
 
 class EmergencyReportController extends Controller
 {
@@ -26,10 +27,13 @@ class EmergencyReportController extends Controller
         $image_path = 'img/emergency-report/';
         $file_name = $image_path . Carbon::now()->timestamp . '.' . $file->getClientOriginalExtension();
         $file->move(public_path($image_path), $file_name);
+        
+        DB::table('users')->where('id', Auth::user()->id)->update([
+            'lat' => $request->lat,
+            'long' => $request->long,
+        ]);
 
         Auth::user()->emergencyReport()->create([
-            'lat' => $request->lat,
-            'lang' => $request->lang,
             'judul' => $request->judul,
             'isi' => $request->isi,
             'foto' => $file_name
